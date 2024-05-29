7581ae24767019b2c628878987450a61f1fffc4a
turgaykurt <turgaykurt@Turgay-MacBook-Pro.local>
2024-05-29 23:33:34 +0300

    Fix component naming conflict

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -2,7 +2,7 @@ const HomeContent = () => {
     return (
         <section className="homecontent">
             <div className="kapsayici">
-                <h1>V1 Welcome to Our Home Page</h1>
+                <h1>V2 Welcome to Our Home Page</h1>
                 <p>This is the home page content.</p>
             </div>
         </section>
