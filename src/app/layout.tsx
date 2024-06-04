79e0b4c6c66d2f961f4306bd2ddb7574874c577a
turgaykurt <turgaykurt@Turgay-MacBook-Pro.local>
2024-06-04 14:31:43 +0300

    v3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+    subsets: ["latin"],
+    display: "swap",
+});
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -13,7 +19,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={inter.className}>
             <body>
                 <Navbar />
                 {children}
